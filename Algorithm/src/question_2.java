24c780a70a26805d481fe7631f7973e0c5ab01a5
dudqls5271 <49426352+dudqls5271@users.noreply.github.com>
2021-09-11 22:32:05 +0900

    Update question_2.java

--- Algorithm/src/question_2.java
+++ Algorithm/src/question_2.java
@@ -2,7 +2,7 @@
 public class question_2 {
 	public static void main(String[] args) {
 		question_2 reverseString = new question_2();
-		System.out.println(reverseString.soulution1("Hello".toCharArray()));
+		System.out.println(reverseString.soulution2("Hello".toCharArray()));
 		
 	}
 	
@@ -34,5 +34,36 @@ public class question_2 {
 		}
  		return reversedMessage;
 	}
+	
+	
+	// h e l l o
+	// he l lo
+	// 4 - 0 -> 4
+	// 4 - 1 = 3
+	
+	/*
+	 * 가운데이 있는 인덱스는 바꿔줄 필요가 없다.
+	 * 홀수 일때는 가운데 값보다 1개 빼고
+	 * 짝수 일때는 가운데 까지 순회하면 된다.
+	 * 
+	 * 시간복잡도는 O(n)
+	 * 공간복잡도는 O(1) -> 별다른 array를 지정해주지 않고 기존에있던 array을 사용했기에 
+	 * */
+	
+	private char[] soulution2(char[] message) {
+		for(int i = 0; i < message.length/2; i++) {
+			char temp = message[i];
+			message[i] = message[message.length-  1 - i];
+			message[message.length - 1 - i] = temp;
+		}
+ 		return message;
+	}
+	
+	
+	
+	/*
+	 * StringBulider 사용법 학습 하기
+	 * 
+	 */
 	
 }
