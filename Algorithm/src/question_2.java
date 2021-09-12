cef5c2c79c41dc9f2b2d5adce4ffcb88b658f58e
dudqls5271 <49426352+dudqls5271@users.noreply.github.com>
2021-09-12 21:08:11 +0900

    Encoding

--- Algorithm/src/question_2.java
+++ Algorithm/src/question_2.java
@@ -9,21 +9,21 @@ public class question_2 {
 	
 	/*
 	 *  H e l l o -> o l l e h 
-	 *  ���� ó�� ���ڸ� �Ųٷ� ������ �Լ��� �ۼ��ض�
+	 *  위에 처럼 문자를 거꾸로 뒤집는 함수를 작성해라
 	 */
 	
 	
-	/* ��Ģ
+	/* 규칙
 	 * 4 - 4 -> 0
 	 * 4 - 3 -> 1
 	 * 4 - 2 -> 2
 	 * 4 - 1 -> 3
 	 * 4 - 0 -> 4
 	 * 
-	 *  message.length-1 => length�� 5������ -1�� ����� �Ѵ�.
+	 *  message.length-1 => length는 5임으로 -1을 해줘야 한다.
 	 *  
-	 *  �ð����⵵�� O(n) -> ������ ��ȸ�ؾ��ϴϱ� 
-	 *  �������⵵�� O(n) -> �迭�� 1�� ��������ϱ�
+	 *  시간복잡도는 O(n) -> 루프를 순회해야하니깐 
+	 *  공간복잡도는 O(n) -> 배열을 1개 만들었으니깐
 	 */
 	
 	private char[] soulution1(char[] message) {
@@ -42,12 +42,12 @@ public class question_2 {
 	// 4 - 1 = 3
 	
 	/*
-	 * ����� �ִ� �ε����� �ٲ��� �ʿ䰡 ����.
-	 * Ȧ�� �϶��� ��� ������ 1�� ����
-	 * ¦�� �϶��� ��� ���� ��ȸ�ϸ� �ȴ�.
+	 * 가운데이 있는 인덱스는 바꿔줄 필요가 없다.
+	 * 홀수 일때는 가운데 값보다 1개 빼고
+	 * 짝수 일때는 가운데 까지 순회하면 된다.
 	 * 
-	 * �ð����⵵�� O(n)
-	 * �������⵵�� O(1) -> ���ٸ� array�� ���������� �ʰ� �������ִ� array�� ����߱⿡ 
+	 * 시간복잡도는 O(n)
+	 * 공간복잡도는 O(1) -> 별다른 array를 지정해주지 않고 기존에있던 array을 사용했기에 
 	 * */
 	
 	private char[] soulution2(char[] message) {
@@ -62,7 +62,26 @@ public class question_2 {
 	
 	
 	/*
-	 * StringBulider ���� �н� �ϱ�
+	 * StringBuilder 사용법 학습 하기
+	 *
+	 * StringBuilder란 
+	 * String은 소위 불변(immutable)객체라고 한다. 
+	 * 
+	 * String str1 = "abc" 
+	 * String str2 = "dfg"
+	 * 이 두개의 String 객체가 있을 때 만약, 
+	 * 
+	 * str1 + str2
+	 * 
+	 * 와 같은 연산을 하게 되면 새로운 String을 생성하게 된다.
+	 * StringBuilder은 이와 같이 새로운 String의 생성을 막기 위해 사용하는것이다.
+	 * 
+	 * StringBuilder는 String과 문자열을 더할 때 새로운 객체를 생성하는 것이 아니라
+	 * 기존의 데이터에 더하는 방식을 사용하기 때문에 속도고 빠르며 상대적으로 부하가 적다
+	 * 
+	 * 따라서 긴 문자열을 더하는 상황이 발생할 경우 StringBuilder을 적극적으로 사용하면 된다.
+	 * 
+	 * 
 	 * 
 	 */
 	
---
+++ Algorithm/src/question_3.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class question_3 {
+
+	public static void main(String[] args) {
+		question_3 twoSumSolutions = new question_3();
+		System.out.println(Arrays.toString(twoSumSolutions.solution2(new int[]{2,3,5,7}, 9)));
+		
+	}
+
+	
+	/*
+	 * 숫자로 구성된 배열 nummers와 target 숫자가 하나가 주어졌을때 nummders 배열에 들어있는 두
+	 * 수를 더해 target 숫자를 만들 수 있는 인덱스 두개를 찾는 함수를 작성하라.
+	 * 
+	 * Ex) nummbers = [2,3,5,7] target = 8
+	 * 8을 만들 수 있는 3과 5의 인덱스인 1,2를 리턴해야 한다.
+	 * Ex) nummbers = [1,2,6,8] target = 9
+	 * 9를 만들 수 있는 1과 8의 인덱스인 0,3를 리턴해야한다.
+	 */
+	
+	
+	/*
+	 * 루프를 2번 돌았으니깐
+	 * 공간복잡도 : O(n²)
+	 * 
+	 * 공간복잡도는 배열 1개 정도이다.
+	 * 하지만 배열이 들어가있는 갯수가 항상 2개로 고정이 되어있기에
+	 * 시간복잡도 : O(1)
+	 * */
+	
+	private int[] solution1(int[] numbers, int target) {
+		for(int i = 0; i < numbers.length; i++) {
+			for(int j = i + 1; j < numbers.length; j++) {
+				if (numbers[i] + numbers[j] == target) {
+					return new int[] {i, j};
+				}
+			}
+		}
+		return null;
+	}
+	
+	
+	
+	
+	/*
+	 * Map을 채워서 O(n)이 들어갔고 get하고 뭐 한거는 O(1)임으로 신경을 쓸필요가 없다.
+	 * 시간복잡도 : O(n)
+	 * 
+	 * Map을 1개더 만들었으니깐
+	 * 공간복잡도 : O(n)
+	 */
+	
+	private int[] solution2(int[] numbers, int target) {
+		
+		// Map 선언
+		Map<Integer, Integer> numberMap = new HashMap<>();
+		
+		
+		// Map에 넣는 과정이다. 
+		// 값을 Key로 넣고, values에다가 index을 넣는다.
+//		for (int i = 0; i< numbers.length; i++) {
+//			numberMap.put(numbers[i], i);
+//		}
+		
+		for(int i = 0; i < numbers.length; i++) {
+			// target은 9 현제 첫번째에 있는 숫자는 2
+			// 그럼으로 2 + x = 9을 성립하며
+			// 9 - 2을 하면 x 가 나온다.
+			int numbersToFind = target - numbers[i];
+			
+			// 2 3 4 5 -> 6을 찾아야 한다면 하면
+			// 위에서 3을 2번 꺼내면 안되기 때문에 
+			// (numberMap.containsKey(numbersToFind) && numberMap.get(numbersToFind) != i) 
+			// 와 같은 if문을 추가 해준 것이다.
+			
+			if (numberMap.containsKey(numbersToFind) && numberMap.get(numbersToFind) != i) {
+				return new int[] {i, numberMap.get(numbersToFind)};
+ 			}
+			
+			// x가 Key로 가지고 있다면 ~ 
+			numberMap.put(numbers[i], i);
+		}
+		return null;
+	}
+	
+	
+	
+	/*
+	 * 위의 문제들을 3sum 문제로 변경하여 제출
+	 * */
+}
