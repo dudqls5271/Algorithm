4268aabe8867a06799401554ae8c091166aa8b3f
dudqls5271 <49426352+dudqls5271@users.noreply.github.com>
2021-09-16 23:56:00 +0900

    Update question_4.java

--- Algorithm/src/question_4.java
+++ Algorithm/src/question_4.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
 
 public class question_4 {
-
+	public static void main(String[] args) {
+		question_3 twoSumSolutions = new question_3();
+		System.out.println(Arrays.toString(twoSumSolutions.solution2(new int[]{2,3,5,7,4}, 9)));
+		
+	}
 }
