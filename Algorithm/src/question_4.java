f36a03b7a66b6acbbe392cfcd35884e919289e78
이영빈 <49426352+dudqls5271@users.noreply.github.com>
2021-09-21 23:21:58 +0900

    Update question_4.java

--- Algorithm/src/question_4.java
+++ Algorithm/src/question_4.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class question_4 {
 	public static void main(String[] args) {
 		question_3 twoSumSolutions = new question_3();
-		System.out.println(Arrays.toString(twoSumSolutions.solution2(new int[]{2,3,5,7,4}, 9)));
+		System.out.println(Arrays.toString(twoSumSolutions.solution2(new int[]{2,3,5,7,4,9})));
 		
 	}
 }
