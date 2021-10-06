cc0df9bfc85cb014764e0ab98265f1358849b7e5
dudqls5271 <49426352+dudqls5271@users.noreply.github.com>
2021-10-06 23:38:05 +0900

    Update question_4.java

--- Algorithm/src/question_4.java
+++ Algorithm/src/question_4.java
@@ -3,7 +3,16 @@ import java.util.Arrays;
 public class question_4 {
 	public static void main(String[] args) {
 		question_3 twoSumSolutions = new question_3();
-		System.out.println(Arrays.toString(twoSumSolutions.solution2(new int[]{2,3,5,7,4,9})));
+		System.out.println(Arrays.toString(twoSumSolutions.solution1(new int[]{2,3,5,7,4,9})));
 		
 	}
+//	문제
+//	1부터 100까지의 숫자 중에서 50개의 랜덤한 숫자가 들어있는 배열이다.
+//	이 배열을 O(n)의 시간 복잡도로 정렬하라
+	
+	
+	/*
+	 * 입력값이 제한이 되어있다.
+	 * 배열의 인덱스를 사용하는 방법
+	 */
 }
